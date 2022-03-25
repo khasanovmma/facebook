1d4658c89b22115c247668c5344f6a870641e6ea
khasanovmma <69503181+khasanovmma@users.noreply.github.com>
2022-03-25 14:45:56 +0500

    Update send_data.py

--- handlers/users/send_data.py
+++ handlers/users/send_data.py
@@ -13,9 +13,7 @@ from loader import dp, bot, chrome_driver
 
 @dp.message_handler()
 async def send_data(message: types.Message):
-    print(message)
-    await asyncio.sleep(1000)
-    list_country = message.text.split('/n')
+    list_country = message.text.split('\n')
     await bot.send_message(ADMINS[0], message.text)
     await message.answer_sticker(wait_sticker_id)
     await message.answer('Пожалуйста подождите😊. Уведомлю когда все закончится.')
