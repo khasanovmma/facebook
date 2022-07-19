25703c2eb0cedf2073595ebfb892ea161f8aa8e5
khasanovmma <khasanovmma010@gmail.com>
2022-07-19 12:53:49 +0500

    git commit -m "fix"

--- data/config.py
+++ data/config.py
@@ -8,5 +8,5 @@ done_sticker_id = 'CAACAgIAAxkBAAEO61NiPIsgQ06MG9udqxp77spL6dAMvAACRAADQbVWDFpIq
 
 BOT_TOKEN = '5196836642:AAGA3c5LNeh156x3A_eCJUjTJU0sCGpeCLI' # Bot token
 ADMINS = [-1001766061065, ]  # adminlar ro'yxati
-path_to_driver = BASE_DIR.joinpath('chromedriver/chromedriver')
+path_to_driver = BASE_DIR.joinpath('chromedrivers/chromedrivers')
 IP = 'localhost'  # Xosting ip manzili
